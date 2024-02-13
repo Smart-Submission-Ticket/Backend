6a5bf3c5ec03cb8db47715b1c613bee37b446a35
GopalSaraf <sarafgopal37@gmail.com>
2024-02-14 01:35:21 +0530

    Added fetch APIs

--- routes/fetch.js
+++ routes/fetch.js
@@ -1,14 +1,19 @@
 const express = require("express");
 
 const { AllowedStudents } = require("../models/allowed_students");
+const { Curriculum } = require("../models/curriculum");
+const { Attendance } = require("../models/attendance");
+
 const teacher = require("../middleware/teacher");
 const {
   getStudentsSpreadSheetValues,
+  getCurriculumSpreadSheetValues,
+  getAttendanceSpreadSheetValues,
 } = require("../utils/googleSheetsService");
 
 const router = express.Router();
 
-router.get("/allowed-students", teacher, async (req, res) => {
+router.get("/students", teacher, async (req, res) => {
   const students = await getStudentsSpreadSheetValues();
   const allowedStudents = await AllowedStudents.find();
 
@@ -31,11 +36,99 @@ router.get("/allowed-students", teacher, async (req, res) => {
 
   await AllowedStudents.insertMany(newStudents);
 
-  res.send({
-    message: "Allowed students updated.",
-    newStudents: newStudents,
-    allowedStudents: allowedStudents,
+  res.send({ message: "Allowed students updated." });
+});
+
+router.get("/curriculum", teacher, async (req, res) => {
+  const curriculum = await getCurriculumSpreadSheetValues();
+  await Curriculum.deleteMany();
+
+  const newCurriculum = [];
+
+  let batch = "";
+  let subjectsTitle = [];
+  let subjectsNoOfAssignments = [];
+  let subjectsTeacher = [];
+
+  for (let i = 0; i < curriculum.length; i++) {
+    if (curriculum[i][0] !== "") {
+      if (batch !== "") {
+        newCurriculum.push({
+          batch,
+          subjects: subjectsTitle.map((title, index) => ({
+            title,
+            noOfAssignments: subjectsNoOfAssignments[index],
+            teacher: subjectsTeacher[index],
+          })),
+        });
+
+        subjectsTitle = [];
+        subjectsNoOfAssignments = [];
+        subjectsTeacher = [];
+      }
+
+      batch = curriculum[i][0];
+    } else {
+      batch = batch;
+    }
+
+    if (curriculum[i][1].trim().toLowerCase().includes("subject"))
+      subjectsTitle = curriculum[i].slice(2);
+
+    if (curriculum[i][1].trim().toLowerCase().includes("assignment"))
+      subjectsNoOfAssignments = curriculum[i].slice(2);
+
+    if (curriculum[i][1].trim().toLowerCase().includes("teacher"))
+      subjectsTeacher = curriculum[i].slice(2);
+  }
+
+  newCurriculum.push({
+    batch,
+    subjects: subjectsTitle.map((title, index) => ({
+      title,
+      noOfAssignments: subjectsNoOfAssignments[index],
+      teacher: subjectsTeacher[index],
+    })),
   });
+
+  await Curriculum.insertMany(newCurriculum);
+
+  res.send({ message: "Curriculum updated." });
+});
+
+router.get("/attendance", teacher, async (req, res) => {
+  const attendance = await getAttendanceSpreadSheetValues();
+  await Attendance.deleteMany();
+
+  const newAttendance = [];
+
+  let rollNos = [];
+  let avgAttendance = [];
+  for (let i = 0; i < attendance.length; i++) {
+    if (attendance[i][1].trim().toLowerCase().includes("roll"))
+      rollNos = attendance[i].slice(2);
+
+    if (attendance[i][1].trim().toLowerCase().includes("attendance")) {
+      avgAttendance = attendance[i].slice(2);
+
+      for (let j = 0; j < rollNos.length; j++) {
+        let _rollNo = rollNos[j];
+        let _attendance = avgAttendance[j];
+
+        if (_rollNo === "" || _attendance === "") continue;
+        _attendance = _attendance.replace("%", "").trim();
+
+        newAttendance.push({
+          rollNo: _rollNo,
+          attendance: _attendance,
+        });
+      }
+    }
+  }
+
+  await Attendance.insertMany(newAttendance);
+
+  res.send({ message: "Attendance updated." });
 });
 
 module.exports = router;
