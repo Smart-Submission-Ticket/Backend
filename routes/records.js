d3c0228766eb5ad677a35947e4f7ca0390156bf2
GopalSaraf <sarafgopal37@gmail.com>
2024-02-21 16:13:21 +0530

    Fixed

--- routes/records.js
+++ routes/records.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const _ = require("lodash");
 
 const { StudentRecord } = require("../models/student_record");
 const { Batch } = require("../models/batch");
+const { AllowedStudents } = require("../models/allowed_student");
 const auth = require("../middleware/auth");
 const teacher = require("../middleware/teacher");
 
@@ -9,13 +11,27 @@ const router = express.Router();
 
 router.get("/", auth, async (req, res, next) => {
   try {
-    const record = await StudentRecord.findOne({
-      rollNo: req.user.rollNo,
-    }).select("-_id -__v");
-
+    const [student, record] = await Promise.all([
+      AllowedStudents.findOne({ rollNo: req.user.rollNo }).select("-_id -__v"),
+      StudentRecord.findOne({ rollNo: req.user.rollNo }).select("-_id -__v"),
+    ]);
     if (!record) return res.status(404).send({ message: "Record not found" });
 
-    res.send(record);
+    const unitTests = {};
+    for (const [key, value] of record.unitTests) {
+      unitTests[key] = _.pick(value, [
+        "ut1",
+        "ut2",
+        "ut1Alternate",
+        "ut2Alternate",
+      ]);
+    }
+
+    res.send({
+      ..._.pick(student, ["rollNo", "email", "batch", "class", "year"]),
+      ..._.pick(record, ["attendance", "attendanceAlternate", "assignments"]),
+      unitTests,
+    });
   } catch (error) {
     console.log(error);
     next(error);
@@ -28,10 +44,27 @@ router.get("/rollNo/:rollNo", teacher, async (req, res, next) => {
     if (!rollNo)
       return res.status(400).send({ message: "Roll no not provided" });
 
-    const record = await StudentRecord.findOne({ rollNo }).select("-_id -__v");
+    const [student, record] = await Promise.all([
+      AllowedStudents.findOne({ rollNo }).select("-_id -__v"),
+      StudentRecord.findOne({ rollNo }).select("-_id -__v"),
+    ]);
     if (!record) return res.status(404).send({ message: "Record not found" });
 
-    res.send(record);
+    const unitTests = {};
+    for (const [key, value] of record.unitTests) {
+      unitTests[key] = _.pick(value, [
+        "ut1",
+        "ut2",
+        "ut1Alternate",
+        "ut2Alternate",
+      ]);
+    }
+
+    res.send({
+      ..._.pick(student, ["rollNo", "email", "batch", "class", "year"]),
+      ..._.pick(record, ["attendance", "attendanceAlternate", "assignments"]),
+      unitTests,
+    });
   } catch (error) {
     console.log(error);
     next(error);
@@ -44,15 +77,41 @@ router.get("/batch/:batch", teacher, async (req, res, next) => {
     if (!batch) return res.status(400).send({ message: "Batch not provided" });
 
     const batchDoc = await Batch.findOne({ batch }).select("-_id -__v");
-
     if (!batchDoc) return res.status(404).send({ message: "Batch not found" });
 
-    const records = await StudentRecord.find({
-      rollNo: { $in: batchDoc.rollNos },
-    }).select("-_id -__v");
-
+    const [students, records] = await Promise.all([
+      AllowedStudents.find({ rollNo: { $in: batchDoc.rollNos } }).select(
+        "-_id -__v"
+      ),
+      StudentRecord.find({ rollNo: { $in: batchDoc.rollNos } }).select(
+        "-_id -__v"
+      ),
+    ]);
     if (!records) return res.status(404).send({ message: "Records not found" });
-    res.send(records);
+
+    const response = {};
+
+    students.forEach((student) => {
+      const record = records.find((record) => record.rollNo === student.rollNo);
+
+      const unitTests = {};
+      for (const [key, value] of record.unitTests) {
+        unitTests[key] = _.pick(value, [
+          "ut1",
+          "ut2",
+          "ut1Alternate",
+          "ut2Alternate",
+        ]);
+      }
+
+      response[student.rollNo] = {
+        ..._.pick(student, ["email", "batch", "class", "year"]),
+        ..._.pick(record, ["attendance", "attendanceAlternate", "assignments"]),
+        unitTests,
+      };
+    });
+
+    res.send(response);
   } catch (error) {
     console.log(error);
     next(error);
