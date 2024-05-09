e375bfaacac8a0995b19dd476ada323c54196f54
GopalSaraf <sarafgopal37@gmail.com>
2024-05-09 23:03:38 +0530

    Temp - Testing IP headers

--- index.js
+++ index.js
@@ -12,15 +12,24 @@ app.get("/", (_, res) => res.status(200).send("Smart Submission Ticket API"));
 require("./startup/routes")(app);
 
 app.get("/ip", (req, res) => {
-  const ip =
-    req.headers["cf-connecting-ip"] ||
-    req.headers["x-real-ip"] ||
-    req.headers["x-forwarded-for"] ||
-    req.socket.remoteAddress ||
-    req.ip ||
-    "";
-  const geoIp = req.useragent.geoIp;
-  res.status(200).send({ ip, geoIp });
+  const cf_connecting_ip = req.headers["cf-connecting-ip"] || "";
+  const Cf_Connecting_Ip = req.headers["Cf-Connecting-Ip"] || "";
+  const x_real_ip = req.headers["x-real-ip"] || "";
+  const x_forwarded_for = req.headers["x-forwarded-for"] || "";
+  const X_Forwarded_For = req.headers["X-Forwarded-For"] || "";
+  const remote_address = req.socket.remoteAddress || "";
+  const ip = req.ip || "";
+  const geoIp = req.useragent.geoIp || "";
+  res.status(200).send({
+    cf_connecting_ip,
+    Cf_Connecting_Ip,
+    x_real_ip,
+    x_forwarded_for,
+    X_Forwarded_For,
+    remote_address,
+    ip,
+    geoIp,
+  });
 });
 
 const server = app.listen(PORT, () =>
